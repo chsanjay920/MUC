acab605b356d8183259d93691c27fc3515119d64
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2021-09-13 18:24:09 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function sankalp(){
+    document.getElementById("sankalp").style.display = "block";
+    document.getElementById("aikyam").style.display = "none";
+    document.getElementById("firefiles").style.display = "none";
+}
+
+
+function aikyam(){
+    document.getElementById("aikyam").style.display = "block";
+    document.getElementById("firefiles").style.display = "none";
+    document.getElementById('sankalp').style.display="none";
+}
+
+function firefiles(){
+    document.getElementById("firefiles").style.display = "block";
+    document.getElementById("aikyam").style.display="none";
+    document.getElementById('sankalp').style.display="none";
+}
+// window.alert("hello");
